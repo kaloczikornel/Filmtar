14ef1c7bf259e2f5ba5a8daad24f877974a11882
kaloczikornel <79595820+kaloczikornel@users.noreply.github.com>
2021-05-29 12:19:26 +0200

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/prog2nhf.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/prog2nhf.dir/Fajlkezelo.cpp.obj"
+  "CMakeFiles/prog2nhf.dir/Filmtar.cpp.obj"
+  "CMakeFiles/prog2nhf.dir/main.cpp.obj"
+  "libprog2nhf.dll.a"
+  "prog2nhf.exe"
+  "prog2nhf.exe.manifest"
+  "prog2nhf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/prog2nhf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
